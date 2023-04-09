bdbea25e95bec0a1b7cbeaed9103fb695dc6740a
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-04-09 18:37:26 +0200

    make update route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy]
   def index
     @tasks = Task.where(user: current_user)
   end
@@ -17,7 +18,26 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :tasks, only: [:new, :create]
+  resources :tasks, only: [:new, :create, :edit, :update, :destroy]
 end
