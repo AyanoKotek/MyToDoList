ae43b2e85fbbfed974b9494808c2614ccd0e4fc5
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-04-09 13:45:11 +0200

    made routes for tasks#index,new,and create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,25 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.where(user: current_user)
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.user = current_user
+    if @task.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
+  end
 end
