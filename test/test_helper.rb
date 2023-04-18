115828a29f22515612aa72d88847ac7430e10bc0
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-04-19 00:21:24 +0200

    try to fix the error and added capybara settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require 'capybara/rails'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -13,4 +14,11 @@ class ActiveSupport::TestCase
   include Warden::Test::Helpers
   Warden.test_mode!
 end
+
+#Register the new driver for Capybara
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
 Capybara.save_path = Rails.root.join("tmp/capybara")
+Capybara.javascript_driver = :headless_chrome
