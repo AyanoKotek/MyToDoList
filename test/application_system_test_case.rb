daf9f2130c713642b2acd1e91fa712ebc7ffe36b
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-04-19 23:47:22 +0200

    Install chromium driver to run the test but still error occurs when run rails test:system

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 #Make sure drivers dont fail under parallel testing
-WebDrivers::Chromedriver.update
+Webdrivers::Chromedriver.update
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
