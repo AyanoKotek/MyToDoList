d340173e5147b1d30fa49cea1c4f994d4390f1d2
Ayano Umemoto <umemoto.ayano@gmail.com>
2023-04-29 14:12:40 +0900

    implement check action but works only for the first task

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -2,14 +2,24 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="checkbox"
 export default class extends Controller {
-  static targets = ["box"];
+  static targets = ["box", "form"];
   connect() {
     console.log(this.boxTarget);
+    console.log(this.formTarget);
   }
 
   check(event) {
-    event.preventDefault();
-
     console.log("TODO: change the box to checked");
+    // console.log(event.currentTarget);
+    // console.log(event.target);
+    const checkedTag =
+      '<i class="fa-regular fa-square-check checkbox"  data-checkbox-target="box" data-action="click->checkbox#check"></i>';
+    const uncheckedTag =
+      '<i class="fa-regular fa-square checkbox" data-checkbox-target="box" data-action="click->checkbox#check"></i>';
+    if (this.boxTarget.outerHTML === uncheckedTag) {
+      this.boxTarget.outerHTML = checkedTag;
+    } else {
+      this.boxTarget.outerHTML = uncheckedTag;
+    }
   }
 }
